e010353f07c4e717a7d871762ae11a1d180fe35c
bang007112 <bang007112@gmail.com>
2021-08-22 13:48:22 +0700

    dishdetail

---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from './shared/dist';
+
+
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent implements OnInit {
+    @Input()
+    dish!: Dish;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
